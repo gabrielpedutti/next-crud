029d88c232e3aede1ea6b31acceefe801d6fc0fa
gabrielpedutti <gabrielpedutti@hotmail.com>
2023-01-12 11:52:37 -0300

    Primeiro commit next-crud

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+content: [
+
+"./src/pages/**/*.{js,ts,jsx,tsx}",
+
+"./src/components/**/*.{js,ts,jsx,tsx}",
+
+],
+
+safelist: [
+    {
+        pattern: /from-(green|blue|gray)-400/,
+    },
+    {
+        pattern: /to-(green|blue|gray)-700/,
+    }
+],
+
+theme: {
+
+extend: {},
+
+},
+
+plugins: [],
+
+}
